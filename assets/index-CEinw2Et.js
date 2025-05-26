d88ec3a9bf47a2c924851d33f51967041e37f5e8
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-05-26 15:46:49 +0900

    Updates

--- assets/index-CEinw2Et.js
+++ assets/index-CEinw2Et.js
@@ -28067,7 +28067,7 @@ function App() {
   ] });
 }
 async function enableMocking() {
-  const { worker } = await __vitePreload(() => import("./browser-Dm7Fm2-x.js"), true ? [] : void 0);
+  const { worker } = await __vitePreload(() => import("./browser-BUjuGW0C.js"), true ? [] : void 0);
   const isLocalhost = location.hostname === "localhost";
   return worker.start({
     serviceWorker: {
